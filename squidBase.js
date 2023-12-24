37d9f14b73c498d12ef7618e04313574206bf151
oarabiledev <153186345+oarabiledev@users.noreply.github.com>
2023-12-25 00:44:19 +0200

    Update squidBase.js

--- squidBase.js
+++ squidBase.js
@@ -1,74 +1,86 @@
-class SquidBase {
-    constructor() {
-        this.elements = [];
-    }
+const ui = {
+    elements: []
+};
 
-    addLayout(type, params) {
-        const layout = document.createElement('div');
-        this.applyLayoutStyles(layout, type, params);
-        this.elements.push(layout);
-        return layout;
-    }
+ui.createLayout = function(type, options) {
+    const layout = document.createElement('div');
+    this.applyLayoutStyle(layout, type, options);
+    this.elements.push(layout);
+    return layout;
+};
 
-    addButton(btnName, width, height, parentLayout, margins) {
-        const button = new SquidButton(btnName);
-        button.setSize(width, height);
-        button.setMargins(margins);
-        parentLayout.appendChild(button.element);
-        return button;
-    }
-
-    applyLayoutStyles(layout, type, params) {
-        if (type === 'Linear') {
-            layout.style.display = 'flex';
-            if (params === 'FillXY') {
-                layout.style.width = '100%';
-                layout.style.height = '100%';
-            }
+ui.applyLayoutStyle = function(layout, type, options) {
+    if (type === 'Linear') {
+        layout.style.display = 'flex';
+        if (options.includes('FillXY')) {
+            layout.style.width = '100%';
+            layout.style.height = '100%';
         }
     }
-
-    render(containerId) {
-        const container = document.getElementById(containerId);
-
-        if (!container) {
-            console.error(`Container with id '${containerId}' not found.`);
-            return;
-        }
-
-        this.elements.forEach(element => {
-            container.appendChild(element);
-        });
+    if (options.includes('left')|| options.includes('Left')) {
+        layout.style.justifyContent = 'flex-start';
     }
+    if (options.includes('right') || options.includes('Right')) {
+        layout.style.justifyContent = 'flex-end';
+    }
+    if (options.includes('center')|| options.includes('Center')) {
+        layout.style.justifyContent = 'center';
+    }
+    if (options.includes('VCenter') || options.includes('vcenter')) {
+        layout.style.alignItems = 'center';
+    }
+    if (options.includes('HCenter') || options.includes('hcenter')) {
+        layout.style.justifyContent = 'center';
+        layout.style.alignItems = 'center';
+    }
+    if (options.includes('Bottom')|| options.includes('bottom') ) {
+        layout.style.alignSelf = 'flex-end';
+    }
+};
+
+ui.render = function() {
+    const container = document.getElementById('app-container');
+    this.elements.forEach(element => {
+        container.appendChild(element);
+    });
+};
+
+ui.addButton = function(btnName, width, height, classname, parentLayout) {
+    return new buttonObj(btnName, width, height, classname, parentLayout);
+};
+
+function buttonObj(btnName, width, height, classname, parentLayout) {
+    this.element = drawButton(btnName, width, height, classname, parentLayout, this);
+    this.setMargins = function(left, top, right, bottom) {
+        this.element.style.marginTop = top || 0;
+        this.element.style.marginRight = right || 0;
+        this.element.style.marginBottom = bottom || 0;
+        this.element.style.marginLeft = left || 0;
+    };
+    this.setSize = function(width, height) {
+        this.element.style.width = width + 'px';
+        this.element.style.height = height + 'px';
+    };
+    this.setOnTouch = function(onTouch) {
+        this.onTouch = onTouch;
+    };
+};
+
+function drawButton(btnName, width, height, classname, parentLayout, btnObj) {
+    const button = document.createElement('button');
+    button.textContent = btnName;
+    if (btnObj instanceof buttonObj) {
+        // Apply the button style class for dynamically created buttons
+        button.className = classname;
+    }
+    button.style.width = width + 'px';
+    button.style.height = height + 'px';
+    button.addEventListener('click', function() {
+        if (btnObj.onTouch) {
+            btnObj.onTouch();
+        }
+    });
+    parentLayout.appendChild(button);
+    return button;
 }
 
-class SquidButton {
-    constructor(btnName) {
-        this.element = document.createElement('button');
-        this.element.textContent = btnName;
-    }
-
-    setMargins(margins) {
-        if (margins) {
-            const { top, right, bottom, left } = margins;
-            this.element.style.marginTop = top || 0;
-            this.element.style.marginRight = right || 0;
-            this.element.style.marginBottom = bottom || 0;
-            this.element.style.marginLeft = left || 0;
-        }
-    }
-
-    setSize(width, height) {
-        if (width) {
-            this.element.style.width = typeof width === 'number' ? `${width * 100}%` : width;
-        }
-
-        if (height) {
-            this.element.style.height = typeof height === 'number' ? `${height * 100}%` : height;
-        }
-    }
-
-    setOnTouch(callback) {
-        this.element.addEventListener('click', callback);
-    }
-}
