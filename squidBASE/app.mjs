3db557b431ab3f09053f995331e7192db2488a1a
Oarabile Koore <153186345+oarabiledev@users.noreply.github.com>
2024-01-04 16:58:18 +0200

    Update app.mjs
    Signed-off-by: Oarabile Koore <153186345+oarabiledev@users.noreply.github.com>

--- squidBASE/app.mjs
+++ squidBASE/app.mjs
@@ -1,18 +1,19 @@
-import {ui} from './Modules/Native Design/main.mjs';
+import { ui } from'./App_Core/main.mjs'
 
+var textSearch;
 function onStart(){
-    const lay = ui.addLayout("Linear","FillXY",null,null,'main');
-   
+    const lay = ui.addLayout('Linear','FillXY,HCenter,Vertical',null,null,'main');
     
+    const btn = ui.addButton('Begin Your Search For squidBASE Packages 🤖',380,50,'search',null,lay)
+    btn.setOnTouch(()=>{
+        btn.animate('animate__backOutDown',()=>{
+            textSearch = ui.addTextField('text','Find Your squidBASE Packages 👻',380,50,'search',null,lay);
+            textSearch.animate('animate__backInDown');
+        });
+    });
     ui.render();
 }
 
-function appEvent(ev){
-    
-}
-
-
-
 document.addEventListener('DOMContentLoaded',()=>{
     onStart();
-});
+})
